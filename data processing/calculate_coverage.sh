2c3fc749c582cd576b6a9db02e8dc7eaa457cb85
len <144913455+pageale@users.noreply.github.com>
2025-05-14 16:10:55 +0200

    Add files via upload

---
+++ data processing/calculate_coverage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+calc_coverage() {
+  BAM=$1
+  echo "Processing '$BAM'..."
+  STATS=$(samtools depth -a "$BAM" | awk '{sum+=$3} END { print sum/NR }')
+  echo -e "${BAM}\t${STATS}"
+  echo "End '$BAM'"
+
+}
+
+export -f calc_coverage
+
+find . -name "*.bam" | parallel -j 10 calc_coverage > bam_coverage_all.txt
